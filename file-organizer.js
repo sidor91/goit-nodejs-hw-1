6202c10758055b647d90b8f516593f380464f5c5
Serhii Sydorenko <sidorchik91@gmail.com>
2026-07-20 17:14:42 +0200

    duplicatesFinder is done

--- file-organizer.js
+++ file-organizer.js
@@ -1,4 +1,5 @@
 import scan from "./lib/scanner.js";
+import duplicateFinder from "./lib/duplicateFinder.js";
 
 const command = process.argv[2];
 const targetPath = process.argv[3];
@@ -6,7 +7,7 @@ const targetPath = process.argv[3];
 if (command === "scan") {
 	scan(targetPath);
 } else if (command === "duplicates") {
-	console.log("Функція пошуку дублікатів ще не реалізована");
+	duplicateFinder(targetPath);
 } else {
 	console.error("❌ Помилка: Невідома команда або команду не вказано.");
 	console.log("Використання: node file-organizer.js <команда> <шлях>");
